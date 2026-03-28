e6b95019239a4848b54211f10ea93b9d3c2fe588
pavankalyanbashaboina-byte <pavankalyanbashaboina@gmail.com>
2026-03-28 23:24:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -531,4 +531,5 @@ def update_report_status(report_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
